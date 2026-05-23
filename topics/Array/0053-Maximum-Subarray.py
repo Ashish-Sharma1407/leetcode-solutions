a2821c67f894eb87662db86506f4269f65d431d2
CodeWithAshish <ashish.sharma.engineering@gmail.com>
2026-05-23 23:31:00 +0530

    ⚡ Optimized: Maximum Subarray (46 → 90)

--- topics/Array/0053-Maximum-Subarray.py
+++ topics/Array/0053-Maximum-Subarray.py
@@ -3,18 +3,17 @@
 # ID       : 53
 # Difficulty: Medium
 # Tags     : Array, Divide and Conquer, Dynamic Programming
-# Runtime  : 46
-# Memory   : 31488000
+# Runtime  : 90
+# Memory   : 32952000
 # Language : Python3
-# Solved   : 2026-05-22 14:45
+# Solved   : 2025-11-15 20:12
 # URL      : https://leetcode.com/problems/maximum-subarray/
 ############################################################
 class Solution:
     def maxSubArray(self, nums: List[int]) -> int:
-        currsum = nums[0]
-        maxsum = nums[0]
-        for i in range(1,len(nums)):
-            currsum = max(nums[i], currsum + nums[i])
-            maxsum = max(currsum, maxsum)
-        return maxsum
-        
+        curr_sum = nums[0]
+        max_sum = nums[0]
+        for i in range (1, len(nums)):
+            curr_sum = max(nums[i], curr_sum + nums[i])
+            max_sum = max(max_sum, curr_sum)
+        return max_sum
