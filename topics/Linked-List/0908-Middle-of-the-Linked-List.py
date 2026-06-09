1a6a1bcb5008a1a928cd74af8a01d17f9a4779d4
CodeWithAshish <ashish.sharma.engineering@gmail.com>
2026-06-10 00:03:18 +0530

    ✅ Added: Middle of the Linked List [Easy]

---
+++ topics/Linked-List/0908-Middle-of-the-Linked-List.py
@@ -0,0 +1,24 @@
+############################################################
+# Problem  : Middle of the Linked List
+# ID       : 908
+# Difficulty: Easy
+# Tags     : Linked List, Two Pointers
+# Runtime  : 0
+# Memory   : 19324000
+# Language : Python3
+# Solved   : 2026-06-09 18:18
+# URL      : https://leetcode.com/problems/middle-of-the-linked-list/
+############################################################
+# Definition for singly-linked list.
+# class ListNode:
+#     def __init__(self, val=0, next=None):
+#         self.val = val
+#         self.next = next
+class Solution:
+    def middleNode(self, head: Optional[ListNode]) -> Optional[ListNode]:
+        slow, fast = head, head
+        while fast and fast.next:
+            slow = slow.next
+            fast = fast.next.next
+        return slow
+        
