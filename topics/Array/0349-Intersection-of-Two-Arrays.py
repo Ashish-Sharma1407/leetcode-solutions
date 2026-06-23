75380906a4a2b6e2094642a406df52061cdd98dd
CodeWithAshish <ashish.sharma.engineering@gmail.com>
2026-06-23 16:32:13 +0530

    🔄 Updated: Intersection of Two Arrays [Easy]

--- topics/Array/0349-Intersection-of-Two-Arrays.py
+++ topics/Array/0349-Intersection-of-Two-Arrays.py
@@ -4,14 +4,14 @@
 # Difficulty: Easy
 # Tags     : Array, Hash Table, Two Pointers, Binary Search, Sorting
 # Runtime  : 0
-# Memory   : 19312000
+# Memory   : 19360000
 # Language : Python3
-# Solved   : 2026-05-27 16:40
+# Solved   : 2026-06-22 10:21
 # URL      : https://leetcode.com/problems/intersection-of-two-arrays/
 ############################################################
 class Solution:
     def intersection(self, nums1: List[int], nums2: List[int]) -> List[int]:
         set1, set2 = set(nums1), set(nums2)
-        return list(set1 & set2)
+        return list(set1.intersection(set2))
 
         
