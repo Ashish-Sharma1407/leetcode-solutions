1ef8efeacd1fa0bc90486dde205273934b09b2aa
CodeWithAshish <ashish.sharma.engineering@gmail.com>
2026-06-23 16:33:01 +0530

    🔄 Updated: Running Sum of 1d Array [Easy]

--- topics/Array/1603-Running-Sum-of-1d-Array.py
+++ topics/Array/1603-Running-Sum-of-1d-Array.py
@@ -4,16 +4,20 @@
 # Difficulty: Easy
 # Tags     : Array, Prefix Sum
 # Runtime  : 0
-# Memory   : 19340000
+# Memory   : 19356000
 # Language : Python3
-# Solved   : 2026-05-21 22:08
+# Solved   : 2026-06-19 08:49
 # URL      : https://leetcode.com/problems/running-sum-of-1d-array/
 ############################################################
 class Solution:
     def runningSum(self, nums: List[int]) -> List[int]:
+        if len(nums) <= 0:
+            return nums
+        ans = []
         add = 0
         for i in range(len(nums)):
-            add += nums[i]
-            nums[i] = add
-        return nums
+            add = add + nums[i]
+            ans.append(add)
+        return ans
+
         
