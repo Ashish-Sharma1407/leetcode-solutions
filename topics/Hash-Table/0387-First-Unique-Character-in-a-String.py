5e5eb182832fe876e838713399c7dc52f011ef9a
CodeWithAshish <ashish.sharma.engineering@gmail.com>
2026-06-23 16:32:10 +0530

    ⚡ Optimized: First Unique Character in a String (89 → 64)

--- topics/Hash-Table/0387-First-Unique-Character-in-a-String.py
+++ topics/Hash-Table/0387-First-Unique-Character-in-a-String.py
@@ -3,24 +3,29 @@
 # ID       : 387
 # Difficulty: Easy
 # Tags     : Hash Table, String, Queue, Counting
-# Runtime  : 89
-# Memory   : 19636000
+# Runtime  : 64
+# Memory   : 19580000
 # Language : Python3
-# Solved   : 2026-05-28 16:55
+# Solved   : 2026-06-22 10:54
 # URL      : https://leetcode.com/problems/first-unique-character-in-a-string/
 ############################################################
 class Solution:
     def firstUniqChar(self, s: str) -> int:
         dic = {}
-        for i in range(len(s)):
-            if s[i] in dic:
-                dic[s[i]] += 1
+        for char in s:
+            if char in dic:
+                dic[char] += 1
             else:
-                dic.update({s[i]:1})
-
-        for i in range(len(s)):
-            if dic[s[i]] == 1:
-                return i
-
-        return -1
+                dic.update({char: 1})
         
+        ans = ""
+        for key in dic.keys():
+            if dic[key] == 1:
+                ans += key
+                break
+        
+        for i in range(len(s)):
+            if s[i] == ans:
+                return i
+        
+        return -1
