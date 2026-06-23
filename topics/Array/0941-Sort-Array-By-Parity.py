68defcbc635d03146c44577d286a4634fc633ddb
CodeWithAshish <ashish.sharma.engineering@gmail.com>
2026-06-23 16:32:54 +0530

    🔄 Updated: Sort Array By Parity [Easy]

--- topics/Array/0941-Sort-Array-By-Parity.py
+++ topics/Array/0941-Sort-Array-By-Parity.py
@@ -4,18 +4,18 @@
 # Difficulty: Easy
 # Tags     : Array, Two Pointers, Sorting
 # Runtime  : 0
-# Memory   : 19888000
+# Memory   : 19748000
 # Language : Python3
-# Solved   : 2026-05-21 23:32
+# Solved   : 2026-06-19 10:04
 # URL      : https://leetcode.com/problems/sort-array-by-parity/
 ############################################################
 class Solution:
     def sortArrayByParity(self, nums: List[int]) -> List[int]:
+        if len(nums) == 0:
+            return nums
         i = 0
-        for j in range(0,len(nums)):
-            if(nums[j] % 2 == 0):
-                temp = nums[i]
-                nums[i] = nums[j]
-                nums[j] = temp
+        for j in range(len(nums)):
+            if nums[j] % 2 == 0:
+                nums[i], nums[j] = nums[j], nums[i]
                 i += 1
         return nums
