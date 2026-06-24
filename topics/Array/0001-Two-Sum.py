8ab3f2332e6abdba9d848a7d65a8071e8d1ef9b4
CodeWithAshish <ashish.sharma.engineering@gmail.com>
2026-06-24 22:56:08 +0530

    ⚡ Optimized: Two Sum (2 → 4)

--- topics/Array/0001-Two-Sum.py
+++ topics/Array/0001-Two-Sum.py
@@ -3,10 +3,10 @@
 # ID       : 1
 # Difficulty: Easy
 # Tags     : Array, Hash Table
-# Runtime  : 2
-# Memory   : 20492000
+# Runtime  : 4
+# Memory   : 20484000
 # Language : Python3
-# Solved   : 2026-06-22 09:18
+# Solved   : 2026-06-24 17:17
 # URL      : https://leetcode.com/problems/two-sum/
 ############################################################
 class Solution:
@@ -14,9 +14,8 @@ class Solution:
         dic = {}
         for i in range(len(nums)):
             if target - nums[i] in dic:
-                return[dic[target - nums[i]], i]
+                return [i,dic[target - nums[i]]]
             else:
-                dic.update({nums[i]: i})
-        return[-1,-1]
-
+                dic.update({nums[i]:i})
+        return [-1,-1]
         
