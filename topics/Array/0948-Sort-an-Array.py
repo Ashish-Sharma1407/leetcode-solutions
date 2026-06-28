e1b65db3710f15e2708985f312495ce62ee700af
CodeWithAshish <ashish.sharma.engineering@gmail.com>
2026-06-29 02:28:12 +0530

    ✅ Added: Sort an Array [Medium]

---
+++ topics/Array/0948-Sort-an-Array.py
@@ -0,0 +1,15 @@
+############################################################
+# Problem  : Sort an Array
+# ID       : 948
+# Difficulty: Medium
+# Tags     : Array, Divide and Conquer, Sorting, Heap (Priority Queue), Merge Sort, Bucket Sort, Radix Sort, Counting Sort
+# Runtime  : 19
+# Memory   : 26324000
+# Language : Python3
+# Solved   : 2026-06-28 20:37
+# URL      : https://leetcode.com/problems/sort-an-array/
+############################################################
+class Solution:
+    def sortArray(self, nums: List[int]) -> List[int]:
+        nums.sort()
+        return nums
