8d1a8d3eb54f60c34f12e19df3677fc90f13aa79
CodeWithAshish <ashish.sharma.engineering@gmail.com>
2026-06-30 02:37:44 +0530

    🔄 Updated: Merge Sorted Array [Easy]

--- topics/Array/0088-Merge-Sorted-Array.py
+++ topics/Array/0088-Merge-Sorted-Array.py
@@ -4,9 +4,9 @@
 # Difficulty: Easy
 # Tags     : Array, Two Pointers, Sorting
 # Runtime  : 0
-# Memory   : 19436000
+# Memory   : 19292000
 # Language : Python3
-# Solved   : 2026-06-04 20:22
+# Solved   : 2026-06-29 19:43
 # URL      : https://leetcode.com/problems/merge-sorted-array/
 ############################################################
 class Solution:
@@ -14,22 +14,18 @@ class Solution:
         """
         Do not return anything, modify nums1 in-place instead.
         """
-        i = m -1
+        i = m - 1
         j = n - 1
         k = m + n - 1
 
-        while i >= 0 and j >= 0:
-            if nums1[i] > nums2[j]:
+        while j >= 0:
+            if i < 0 or nums1[i] < nums2[j]:
+                nums1[k] = nums2[j]
+                k -= 1
+                j -= 1 
+            else:
                 nums1[k] = nums1[i]
                 i -= 1
-            else:
-                nums1[k] = nums2[j]
-                j -= 1
-            k -= 1
-        
-        while j >= 0:
-            nums1[k] = nums2[j]
-            j -= 1
-            k -= 1
-
+                k -= 1
+                
         
