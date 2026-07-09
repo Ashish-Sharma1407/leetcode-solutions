0b85b6654caada74601b8dfe5ffa98a063998461
CodeWithAshish <ashish.sharma.engineering@gmail.com>
2026-07-09 17:08:17 +0530

    ✅ Added: Binary Tree Preorder Traversal [Easy]

---
+++ topics/Stack/0144-Binary-Tree-Preorder-Traversal.py
@@ -0,0 +1,31 @@
+############################################################
+# Problem  : Binary Tree Preorder Traversal
+# ID       : 144
+# Difficulty: Easy
+# Tags     : Stack, Tree, Depth-First Search, Binary Tree
+# Runtime  : 0
+# Memory   : 19308000
+# Language : Python3
+# Solved   : 2026-07-08 11:57
+# URL      : https://leetcode.com/problems/binary-tree-preorder-traversal/
+############################################################
+# Definition for a binary tree node.
+# class TreeNode:
+#     def __init__(self, val=0, left=None, right=None):
+#         self.val = val
+#         self.left = left
+#         self.right = right
+class Solution:
+    def __init__(self):
+        self.ans = []
+    def preorder(self,root):
+        if root is None:
+            return
+        self.ans.append(root.val)
+        self.preorder(root.left)
+        self.preorder(root.right)
+
+    def preorderTraversal(self, root: Optional[TreeNode]) -> List[int]:
+        self.preorder(root)
+        return self.ans
+        
