a9667d7297bc079c03722c69049bd2816e630919
CodeWithAshish <ashish.sharma.engineering@gmail.com>
2026-07-20 16:51:35 +0530

    ⚡ Optimized: Best Time to Buy and Sell Stock (55 → 47)

--- topics/Array/0121-Best-Time-to-Buy-and-Sell-Stock.py
+++ topics/Array/0121-Best-Time-to-Buy-and-Sell-Stock.py
@@ -3,20 +3,21 @@
 # ID       : 121
 # Difficulty: Easy
 # Tags     : Array, Dynamic Programming
-# Runtime  : 55
-# Memory   : 28552000
+# Runtime  : 47
+# Memory   : 28652000
 # Language : Python3
-# Solved   : 2026-06-19 11:12
+# Solved   : 2026-07-20 09:24
 # URL      : https://leetcode.com/problems/best-time-to-buy-and-sell-stock/
 ############################################################
 class Solution:
     def maxProfit(self, prices: List[int]) -> int:
         buy = prices[0]
         sell = 0
-        for i in range(1,len(prices)):
-            if prices[i] < buy:
+        maxprofit = 0
+        for i in range(1, len(prices)):
+            if prices[i] <= buy:
                 buy = prices[i]
             else:
-                sell = max(sell, prices[i]-buy)
-        return sell
-        
+                sell = prices[i] - buy
+                maxprofit = max(sell, maxprofit)
+        return maxprofit
