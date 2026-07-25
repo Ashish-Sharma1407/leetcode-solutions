4a72154b053be397ef6f22891a772a7c08074c19
CodeWithAshish <ashish.sharma.engineering@gmail.com>
2026-07-26 02:16:42 +0530

    ⚡ Optimized: Rotate Array (6 → 3)

--- topics/Array/0189-Rotate-Array.py
+++ topics/Array/0189-Rotate-Array.py
@@ -3,22 +3,24 @@
 # ID       : 189
 # Difficulty: Medium
 # Tags     : Array, Math, Two Pointers
-# Runtime  : 6
-# Memory   : 26496000
+# Runtime  : 3
+# Memory   : 26528000
 # Language : Python3
-# Solved   : 2026-06-04 10:01
+# Solved   : 2026-07-25 20:20
 # URL      : https://leetcode.com/problems/rotate-array/
 ############################################################
 class Solution:
-    def rev(self,nums,left,right):
+    def rev(self,left,right,nums):
         while left < right:
             nums[left], nums[right] = nums[right], nums[left]
             left += 1
             right -= 1
-
+        
     def rotate(self, nums: List[int], k: int) -> None:
         n = len(nums)
         k = k % n
-        self.rev(nums,n-k,n-1)
-        self.rev(nums,0,n-k-1)
-        self.rev(nums,0,n-1)
+        self.rev(n-k,n-1,nums)
+        self.rev(0,n-k-1,nums)
+        self.rev(0,n-1,nums)
+
+        
