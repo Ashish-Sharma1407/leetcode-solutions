657ab5d0f029a3866aa236550a169a08343005e1
CodeWithAshish <ashish.sharma.engineering@gmail.com>
2026-07-26 22:37:50 +0530

    ⚡ Optimized: Valid Palindrome (11 → 21)

--- topics/Two-Pointers/0125-Valid-Palindrome.py
+++ topics/Two-Pointers/0125-Valid-Palindrome.py
@@ -3,10 +3,10 @@
 # ID       : 125
 # Difficulty: Easy
 # Tags     : Two Pointers, String
-# Runtime  : 11
-# Memory   : 19728000
+# Runtime  : 21
+# Memory   : 19696000
 # Language : Python3
-# Solved   : 2026-05-24 13:58
+# Solved   : 2026-07-26 16:42
 # URL      : https://leetcode.com/problems/valid-palindrome/
 ############################################################
 class Solution:
@@ -15,22 +15,20 @@ class Solution:
         if 97<=x<=122 or 65<=x<=90 or 48<=x<=57:
             return True
         return False
-        
+
     def isPalindrome(self, s: str) -> bool:
         s = s.lower()
-        i = 0
-        j = len(s)-1
-
-        while i < j:
-            if not self.alphanumeric(s[i]):
-                i += 1
-            elif not self.alphanumeric(s[j]):
-                j -= 1
-            elif s[i] == s[j]:
-                i += 1
-                j -= 1
-            else:
-                return False
-        return True
+        s1 = ""
+        s2 = ""
+        for ch in s:
+            ans = self.alphanumeric(ch)
+            if ans:
+                s1 += ch
+        print(s1)
+        for i in range(len(s)-1,-1,-1):
+            ans = self.alphanumeric(s[i])
+            if ans:
+                s2 += s[i]
+        return s1 == s2
 
         
