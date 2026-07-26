d3fe4f01fa22bc38f0ba381d94a55fb084c871f2
CodeWithAshish <ashish.sharma.engineering@gmail.com>
2026-07-27 00:01:54 +0530

    ⚡ Optimized: Two Sum II - Input Array Is Sorted (4 → 0)

--- topics/Array/0167-Two-Sum-II---Input-Array-Is-Sorted.py
+++ topics/Array/0167-Two-Sum-II---Input-Array-Is-Sorted.py
@@ -3,22 +3,21 @@
 # ID       : 167
 # Difficulty: Medium
 # Tags     : Array, Two Pointers, Binary Search
-# Runtime  : 4
-# Memory   : 20540000
+# Runtime  : 0
+# Memory   : 20612000
 # Language : Python3
-# Solved   : 2026-06-22 10:00
+# Solved   : 2026-07-26 17:07
 # URL      : https://leetcode.com/problems/two-sum-ii-input-array-is-sorted/
 ############################################################
 class Solution:
-    def twoSum(self, numbers: List[int], target: int) -> List[int]:
-        st = 0
-        end = len(numbers)-1
-        while st < end:
-            if (numbers[st] + numbers[end]) == target:
-                return [st+1,end+1]
-            elif (numbers[st] + numbers[end]) < target:
-                st += 1
-            elif (numbers[st] + numbers[end]) > target:
+    def twoSum(self, nums: List[int], target: int) -> List[int]:
+        start = 0
+        end = len(nums)-1
+        while(start < end):
+            if nums[start] + nums[end] == target:
+                return [start+1,end+1]
+            elif nums[start] + nums[end] > target:
                 end -= 1
+            else:
+                start += 1
         return [-1,-1]
-        
