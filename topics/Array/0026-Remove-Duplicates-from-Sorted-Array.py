61b25bf9658df2b2bc6ad08b12d1b1e02960887c
CodeWithAshish <ashish.sharma.engineering@gmail.com>
2026-09-11 03:16:30 +0530

    🔄 Updated: Remove Duplicates from Sorted Array [Easy]

--- topics/Array/0026-Remove-Duplicates-from-Sorted-Array.py
+++ topics/Array/0026-Remove-Duplicates-from-Sorted-Array.py
@@ -4,21 +4,17 @@
 # Difficulty: Easy
 # Tags     : Array, Two Pointers
 # Runtime  : 0
-# Memory   : 20484000
+# Memory   : 20436000
 # Language : Python3
-# Solved   : 2026-07-19 18:02
+# Solved   : 2026-09-10 19:35
 # URL      : https://leetcode.com/problems/remove-duplicates-from-sorted-array/
 ############################################################
 class Solution:
     def removeDuplicates(self, nums: List[int]) -> int:
         start = 0
-        end = 0
-        while end < len(nums):
-            if nums[start] != nums[end]:
+        for i in range(1, len(nums)):
+            if nums[start] != nums[i]:
                 start += 1
-                nums[start], nums[end] = nums[end], nums[start]
-            end += 1
+                nums[start],nums[i] = nums[i],nums[start]
         return start + 1
-        
-
         
