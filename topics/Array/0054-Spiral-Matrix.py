775f69587009fdee49384b333f82a997279a048d
CodeWithAshish <ashish.sharma.engineering@gmail.com>
2026-09-11 03:16:05 +0530

    🔄 Updated: Spiral Matrix [Medium]

--- topics/Array/0054-Spiral-Matrix.py
+++ topics/Array/0054-Spiral-Matrix.py
@@ -4,9 +4,9 @@
 # Difficulty: Medium
 # Tags     : Array, Matrix, Simulation
 # Runtime  : 0
-# Memory   : 19416000
+# Memory   : 19372000
 # Language : Python3
-# Solved   : 2026-06-20 14:24
+# Solved   : 2026-09-10 20:29
 # URL      : https://leetcode.com/problems/spiral-matrix/
 ############################################################
 class Solution:
@@ -16,28 +16,26 @@ class Solution:
         re = len(matrix)-1
         cs = 0
         ce = len(matrix[0])-1
-        
+
         while rs <= re and cs <= ce:
 
             for i in range(cs, ce+1):
                 ans.append(matrix[rs][i])
             rs += 1
 
-            for i in range(rs, re+1):
+            for i in range(rs,re+1):
                 ans.append(matrix[i][ce])
             ce -= 1
-            
+
             if rs <= re:
                 for i in range(ce, cs-1, -1):
                     ans.append(matrix[re][i])
                 re -= 1
 
             if cs <= ce:
-                for i in range(re, rs-1, -1):
+                for i in range(re,rs-1,-1):
                     ans.append(matrix[i][cs])
                 cs += 1
         
         return ans
-            
-
-    
+        
