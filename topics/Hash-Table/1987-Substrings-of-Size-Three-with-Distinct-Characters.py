7d18a3de54fdb049849436486651d97df0ffaf40
CodeWithAshish <ashish.sharma.engineering@gmail.com>
2026-09-11 03:15:39 +0530

    🔄 Updated: Substrings of Size Three with Distinct Characters [Easy]

--- topics/Hash-Table/1987-Substrings-of-Size-Three-with-Distinct-Characters.py
+++ topics/Hash-Table/1987-Substrings-of-Size-Three-with-Distinct-Characters.py
@@ -4,19 +4,21 @@
 # Difficulty: Easy
 # Tags     : Hash Table, String, Sliding Window, Counting
 # Runtime  : 0
-# Memory   : 19288000
+# Memory   : 19344000
 # Language : Python3
-# Solved   : 2026-06-23 14:05
+# Solved   : 2026-09-10 21:33
 # URL      : https://leetcode.com/problems/substrings-of-size-three-with-distinct-characters/
 ############################################################
 class Solution:
     def countGoodSubstrings(self, s: str) -> int:
-        if len(s) < 3:
-            return 0
-        p3 = 2
+        prev = 0
+        curr = 1
+        nxt = 2
         cnt = 0
-        while p3 != len(s):
-            if s[p3]!=s[p3-1] and s[p3-2] != s[p3-1] and s[p3-2] != s[p3]:
+        while nxt < len(s):
+            if s[prev] != s[curr] and s[prev] != s[nxt] and s[curr] != s[nxt]:
                 cnt += 1
-            p3 += 1
+            prev += 1
+            curr += 1
+            nxt += 1
         return cnt
