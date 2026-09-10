923ca859585073e156f95c3be2ad6462ebe67079
CodeWithAshish <ashish.sharma.engineering@gmail.com>
2026-09-11 03:16:09 +0530

    🔄 Updated: Richest Customer Wealth [Easy]

--- topics/Array/1791-Richest-Customer-Wealth.py
+++ topics/Array/1791-Richest-Customer-Wealth.py
@@ -4,15 +4,14 @@
 # Difficulty: Easy
 # Tags     : Array, Matrix
 # Runtime  : 0
-# Memory   : 19292000
+# Memory   : 19328000
 # Language : Python3
-# Solved   : 2026-06-19 12:11
+# Solved   : 2026-09-10 20:17
 # URL      : https://leetcode.com/problems/richest-customer-wealth/
 ############################################################
 class Solution:
     def maximumWealth(self, accounts: List[List[int]]) -> int:
-        max_wealth = 0
-        for i in range(len(accounts)):
-            max_wealth = max(max_wealth, sum(accounts[i]))
-        return max_wealth
-        
+        ans = 0
+        for account in accounts:
+            ans = max(ans,sum(account))
+        return ans
