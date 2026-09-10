0d11899ba803392a63d7403def1a4196f183386e
CodeWithAshish <ashish.sharma.engineering@gmail.com>
2026-09-11 03:15:45 +0530

    ⚡ Optimized: Valid Anagram (11 → 12)

--- topics/Hash-Table/0242-Valid-Anagram.py
+++ topics/Hash-Table/0242-Valid-Anagram.py
@@ -3,33 +3,27 @@
 # ID       : 242
 # Difficulty: Easy
 # Tags     : Hash Table, String, Sorting
-# Runtime  : 11
-# Memory   : 19240000
+# Runtime  : 12
+# Memory   : 19456000
 # Language : Python3
-# Solved   : 2026-06-22 11:43
+# Solved   : 2026-09-10 21:21
 # URL      : https://leetcode.com/problems/valid-anagram/
 ############################################################
 class Solution:
     def isAnagram(self, s: str, t: str) -> bool:
-        
         if len(s) != len(t):
             return False
-        
         dic = {}
         for char in s:
             if char in dic:
                 dic[char] += 1
             else:
                 dic.update({char: 1})
-        
         for char in t:
             if char in dic:
                 dic[char] -= 1
-            else:
+        for value in dic.values():
+            if value != 0:
                 return False
+        return True
         
-        if all(val == 0 for val in dic.values()):
-            return True
-        
-        return False
-
