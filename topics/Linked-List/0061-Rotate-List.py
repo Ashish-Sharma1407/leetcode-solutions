213e92943c08bb7d0b07cfc02bc393c3be233648
CodeWithAshish <ashish.sharma.engineering@gmail.com>
2026-09-16 22:41:33 +0530

    🔄 Updated: Rotate List [Medium]

--- topics/Linked-List/0061-Rotate-List.py
+++ topics/Linked-List/0061-Rotate-List.py
@@ -4,9 +4,9 @@
 # Difficulty: Medium
 # Tags     : Linked List, Two Pointers
 # Runtime  : 0
-# Memory   : 19412000
+# Memory   : 19388000
 # Language : Python3
-# Solved   : 2026-07-01 13:21
+# Solved   : 2026-09-11 21:16
 # URL      : https://leetcode.com/problems/rotate-list/
 ############################################################
 # Definition for singly-linked list.
@@ -16,30 +16,29 @@
 #         self.next = next
 class Solution:
     def rotateRight(self, head: Optional[ListNode], k: int) -> Optional[ListNode]:
-        p1 = head
-        p2 = head
 
-        if head == None or head.next == None:
+        if head == None:
             return head
-
-        cnt = 0
+            
         curr = head
-        while curr != None:
+        cnt = 0
+        while curr.next != None:
+            curr = curr.next
             cnt += 1
-            curr = curr.next  
+        cnt += 1
+        
         k = k % cnt
-
         if k == 0:
             return head
+        place = cnt - k
 
-        for i in range(k):
-            p2 = p2.next
-        while p2.next != None:
-            p1 = p1.next
-            p2 = p2.next
-        temp = p1.next
-        p1.next = None
-        p2.next = head
-        return temp
-
+        temp = head
+        for i in range(1,place):
+            temp = temp.next
         
+        curr.next = head
+        head = temp.next
+        temp.next = None
+    
+        return head
+
