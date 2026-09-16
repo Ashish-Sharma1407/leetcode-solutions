dc28bb1911acef5ab410df22d98014078c9147d1
CodeWithAshish <ashish.sharma.engineering@gmail.com>
2026-09-16 22:41:36 +0530

    🔄 Updated: Reverse Linked List [Easy]

--- topics/Linked-List/0206-Reverse-Linked-List.py
+++ topics/Linked-List/0206-Reverse-Linked-List.py
@@ -4,9 +4,9 @@
 # Difficulty: Easy
 # Tags     : Linked List, Recursion
 # Runtime  : 0
-# Memory   : 20432000
+# Memory   : 20368000
 # Language : Python3
-# Solved   : 2026-07-01 11:41
+# Solved   : 2026-09-11 21:03
 # URL      : https://leetcode.com/problems/reverse-linked-list/
 ############################################################
 # Definition for singly-linked list.
@@ -18,14 +18,11 @@ class Solution:
     def reverseList(self, head: Optional[ListNode]) -> Optional[ListNode]:
         prev = None
         curr = head
+        nxt = None
         while curr != None:
             nxt = curr.next
             curr.next = prev
             prev = curr
             curr = nxt
         return prev
-
-
-
-
         
