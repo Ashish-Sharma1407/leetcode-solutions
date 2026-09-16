df3f51c4360499d0d49d54698797006bed0c24de
CodeWithAshish <ashish.sharma.engineering@gmail.com>
2026-09-16 22:41:07 +0530

    ⚡ Optimized: Big Countries (266 → 286)

--- topics/Database/0595-Big-Countries.py
+++ topics/Database/0595-Big-Countries.py
@@ -3,12 +3,14 @@
 # ID       : 595
 # Difficulty: Easy
 # Tags     : Database
-# Runtime  : 266
+# Runtime  : 286
 # Memory   : 0
 # Language : MySQL
-# Solved   : 2025-10-10 13:26
+# Solved   : 2026-09-15 20:46
 # URL      : https://leetcode.com/problems/big-countries/
 ############################################################
 # Write your MySQL query statement below
-Select name, population, area FROM World
-WHERE area >= 3000000 or population >= 25000000;
+SELECT name, population, area
+FROM World
+WHERE area >= 3000000 
+    OR population >= 25000000;
